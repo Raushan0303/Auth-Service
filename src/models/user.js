c15bb92b208087d20495966e793d7b5512a4fcf9
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2024-02-18 11:23:24 +0530

    crud for user

--- src/models/user.js
+++ src/models/user.js
@@ -14,8 +14,18 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   user.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len:[3,300]
+      }
+    },
   }, {
     sequelize,
     modelName: 'user',
